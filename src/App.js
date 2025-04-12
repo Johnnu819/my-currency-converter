0e3d64ef894b32fd8005093230175c76e12669ca
Johnnu819 <volyovale@outlook.com>
2025-04-12 21:03:29 +0100

    changed the text in the currencySelector component and added components to the app

--- src/App.js
+++ src/App.js
@@ -4,18 +4,19 @@ import "./components/currencySelector.js"
 import { useState } from "react";
 import CurrencySelector from './components/currencySelector.js';
 import ConvertButton from './components/convertButton.js';
+import ConverterPanel from './components/converterPanel.js';
 
 function App() {
 
-  const [amount, setAmount] = useState(1);
+  const [amount, setAmount] = useState(0);
   const [fromCurrency, setFromCurrency] = useState("USD");
   const [toCurrency, setToCurrency] = useState("EUR");
   const currencyOptions = ["USD", "EUR", "GBP", "JPY"];
 
   return (
-
-    
-    <ConvertButton on_click = {() => alert("clicked")}/>
+    <div class = "card" style={{width: "30%"}}>
+      <ConverterPanel currency_options={currencyOptions}></ConverterPanel>
+    </div>
   );
 }
 
--- src/components/currencySelector.js
+++ src/components/currencySelector.js
@@ -2,10 +2,11 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const CurrencySelector = ({ label, value, onChange, options }) => {
+  
   return (
     <div className="flex flex-col">
-      <label for="currency-select" class="form-label">Select '{label}' currency</label>
-      <select class="form-select" 
+      <label for="currency-select" class="form-label">Select {label} currency</label>
+      <select class="form-select"  id = "currency_select"
         value={value}
         onChange={(e) => onChange(e.target.value)}
       >
